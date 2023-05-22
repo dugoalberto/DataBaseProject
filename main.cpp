ab28df5142810760534503d53c3963d55e8be89f
dugoalberto <www.da.0502@gmail.com>
2023-05-22 21:17:34 +0200

    worka

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
+//
+// Created by Alberto Dugo on 22/05/23.
+//
+#include <libpq-fe.h>
 #include <iostream>
-#include "dependencies/include/libpq-fe.h"
 
-int main (int argc, char **argv) {
-    PGconn *conn = PQconnectdb("host=<localhost> port=<5432> user=<root> password=<root> dbname=<test_db>");
-    PGresult *res;
-    res = PQexec(conn, "SELECT * FROM hubs");
-    int tuple = PQntuples(res);
-    int campi = PQnfields(res);
-    for (int i = 0; i < campi; ++i) {
-        std::cout << PQfname(res, i) << "\t\t";
+//#include "dependencies/include/libpq-fe.h"
+using namespace std;
+int main(){
+    const char* conninfo = "host=localhost port=5432 dbname=test_db user=root password=root";
+    PGconn * conn = PQconnectdb(conninfo);
+    if(PQstatus(conn) != CONNECTION_OK){
+        cout<<"Errore di connessione "<<PQerrorMessage(conn);
+        PQfinish(conn);
+        exit(1);
+    }else{
+        cout<<"Connessione avvenuta correttamente";
+        PQfinish(conn);
     }
-    std::cout << std::endl;
-    for(int i=0;i<tuple;++i){
-        for (int j = 0; j < campi; ++j) {
-            std::cout << PQgetvalue(res, i, j) << "\t\t";
-        }
-        std::cout << std::endl;
-    }
+    return 0;
 }
