806b13e82b0a57af2160bf3a2029fe870b6d2a48
dugoalberto <www.da.0502@gmail.com>
2023-05-22 21:40:54 +0200

    la mia prima instert e la mia prima query

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/albertodugo/Desktop/provaLab5_1
+# Install script for directory: /Users/albertodugo/Desktop/progettoBasiDiDati
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/albertodugo/Desktop/provaLab5_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/albertodugo/Desktop/progettoBasiDiDati/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -6,16 +6,51 @@
 
 //#include "dependencies/include/libpq-fe.h"
 using namespace std;
-int main(){
-    const char* conninfo = "host=localhost port=5432 dbname=test_db user=root password=root";
-    PGconn * conn = PQconnectdb(conninfo);
-    if(PQstatus(conn) != CONNECTION_OK){
-        cout<<"Errore di connessione "<<PQerrorMessage(conn);
+#include <libpq-fe.h>
+
+int main() {
+    const char* conninfo = "host=localhost port=5432 dbname=postgres user=root password=root";
+    PGconn* conn = PQconnectdb(conninfo);
+    PGresult* res;
+
+    if (PQstatus(conn) != CONNECTION_OK) {
+        printf("Connection to database failed: %s\n", PQerrorMessage(conn));
         PQfinish(conn);
-        exit(1);
-    }else{
-        cout<<"Connessione avvenuta correttamente";
-        PQfinish(conn);
+        return 1;
     }
+
+    for (int i = 0; i < 100; ++i) {
+        char query[100];
+        snprintf(query, sizeof(query), "INSERT INTO hubs(prova) values (%d)", i);
+        res = PQexec(conn, query);
+        if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+            printf("Error executing INSERT query: %s\n", PQerrorMessage(conn));
+            PQclear(res);
+            PQfinish(conn);
+            return 1;
+        }
+        PQclear(res);
+    }
+
+    res = PQexec(conn, "SELECT * FROM hubs");
+    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+        printf("Error executing SELECT query: %s\n", PQerrorMessage(conn));
+        PQclear(res);
+        PQfinish(conn);
+        return 1;
+    }
+
+    int rows = PQntuples(res);
+    int cols = PQnfields(res);
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            printf("%s\t", PQgetvalue(res, i, j));
+        }
+        printf("\n");
+    }
+
+    PQclear(res);
+    PQfinish(conn);
     return 0;
 }
